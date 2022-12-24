2f1a887d4025449bd3187659e85b7c5d1f46e362
Hotmoon <aravind1333.av@gmail.com>
2022-12-24 10:49:59 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -38,11 +38,11 @@ async def save_group(bot, message):
             await bot.leave_chat(message.chat.id)
             return
         buttons = [[
-                    InlineKeyboardButton('Sᴜᴘᴘᴏʀᴛ Gʀᴏᴜᴘ', url=GRP_LNK),
-                    InlineKeyboardButton('Uᴘᴅᴀᴛᴇs Cʜᴀɴɴᴇʟ', url=CHNL_LNK)
-                 ],[
-                    InlineKeyboardButton("Bᴏᴛ Oᴡɴᴇʀ", url="t.me/creatorbeatz")
-                  ]]
+                                                                           InlineKeyboardButton('ʙᴏᴛ ᴏᴡɴᴇʀ', url="t.me/arvinxoxo"),
+                                                                           InlineKeyboardButton('ᴄʜᴇᴄᴋ ᴘᴍ', url="https://t.me/{temp.U_NAME}")
+                                                                        ],[
+                                                                           InlineKeyboardButton("ʀᴇᴀᴅ ɢʀᴏᴜᴘ ʀᴜʟᴇs📌", url="https://graph.org/Bot-of-Mindflayers-Mirror-12-24")
+                                                                         ]]
         reply_markup=InlineKeyboardMarkup(buttons)
         await message.reply_text(
             text=f"<b>Thankyou For Adding Me In {message.chat.title} ❣️\n\nIf you have any questions & doubts about using me contact support.</b>",
@@ -61,13 +61,14 @@ async def save_group(bot, message):
                                                  caption=(script.MELCOW_ENG.format(u.mention, message.chat.title)),
                                                  reply_markup=InlineKeyboardMarkup(
                                                                          [[
-                                                                           InlineKeyboardButton('Sᴜᴘᴘᴏʀᴛ Gʀᴏᴜᴘ', url=GRP_LNK),
-                                                                           InlineKeyboardButton('Uᴘᴅᴀᴛᴇs Cʜᴀɴɴᴇʟ', url=CHNL_LNK)
+                                                                           InlineKeyboardButton('ʙᴏᴛ ᴏᴡɴᴇʀ', url="t.me/arvinxoxo"),
+                                                                           InlineKeyboardButton('ᴄʜᴇᴄᴋ ᴘᴍ', url="https://t.me/{temp.U_NAME}")
                                                                         ],[
-                                                                           InlineKeyboardButton("Bᴏᴛ Oᴡɴᴇʀ", url="t.me/creatorbeatz")
+                                                                           InlineKeyboardButton("ʀᴇᴀᴅ ɢʀᴏᴜᴘ ʀᴜʟᴇs📌", url="https://graph.org/Bot-of-Mindflayers-Mirror-12-24")
                                                                          ]]
                                                  ),
                                                  parse_mode=enums.ParseMode.HTML
+                                                 disable_web_page_preview=True
                 )
                 
         if settings["auto_delete"]:
